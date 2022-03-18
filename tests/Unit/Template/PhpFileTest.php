861fdc727ba431b5d41a248bf53007e8f29f1839
ubermanu <manu@paca.happy-dev.fr>
2022-03-18 15:54:32 +0100

    minor changes

--- tests/Unit/Template/PhpFileTest.php
+++ tests/Unit/Template/PhpFileTest.php
@@ -8,7 +8,7 @@ final class PhpFileTest extends \PHPUnit\Framework\TestCase
      */
     public function testRendersProperly()
     {
-        $filename = dirname(__FILE__) . '/files/php-template.phtml';
+        $filename = dirname(__FILE__) . '/_files/php-template.phtml';
         $template = new \Kek\Template\PhpFile($filename);
 
         $this->assertEquals("<h1>Hello World</h1>\n", $template->render(['someVar' => 'World']));
